ed3e69ff90f9d6c8f4d02196b09d96a582a2dce8
biliman <fickry.bil.iman@gmail.com>
2021-12-24 21:09:59 +0700

    Transform Data to Use MDX

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 import Layout from '../components/layout'
 
 const BlogPage = ({data}) => {
@@ -7,10 +8,14 @@ const BlogPage = ({data}) => {
       <Layout pageTitle="My Blog Posts">
          <ul>
             {
-               data.allFile.nodes.map(node => (
-                  <li key={node.name}>
-                     {node.name}
-                  </li>
+               data.allMdx.nodes.map((node) => (
+                  <article key={node.id}>
+                     <h2>{node.frontmatter.title}</h2>
+                     <p>Posted: {node.frontmatter.date}</p>
+                     <MDXRenderer>
+                        {node.body}
+                     </MDXRenderer>
+                  </article>
                ))
             }
          </ul>
@@ -20,9 +25,14 @@ const BlogPage = ({data}) => {
 
 export const query = graphql`
    query {
-      allFile {
+      allMdx(sort: {fields: frontmatter___date, order: DESC}) {
          nodes {
-            name
+            frontmatter {
+               date(formatString: "MMMM, D YYYY")
+               title
+            }
+            id
+            body
          }
       }
    }
