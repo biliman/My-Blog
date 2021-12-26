c07898fb34170dd795419638e0584b6358da37e9
biliman <fickry.bil.iman@gmail.com>
2021-12-26 20:31:16 +0700

    Add Dynamic images from Data

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,12 +1,25 @@
 import { graphql } from 'gatsby'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 import Layout from '../../components/layout'
 
 const BlogPost = ({data}) => {
+   const image = getImage(data.mdx.frontmatter.hero_image.childImageSharp.gatsbyImageData)
+
    return (
       <Layout pageTitle={data.mdx.frontmatter.title}>
          <p>{data.mdx.frontmatter.date}</p>
+         <GatsbyImage 
+            image={image}
+            alt={data.mdx.frontmatter.hero_image_alt}
+         />
+         <p>
+            Photo Credit: {" "}
+            <a href="{data.mdx.frontmatter.hero_image_credit_link}">
+               {data.mdx.frontmatter.hero_image_credit_text}
+            </a>
+         </p>
          <MDXRenderer>
             {data.mdx.body}
          </MDXRenderer>
@@ -20,6 +33,14 @@ export const query = graphql`
          frontmatter {
             title
             date(formatString: "MMMM D, YYYY")
+            hero_image_alt
+            hero_image_credit_link
+            hero_image_credit_text
+            hero_image {
+               childImageSharp {
+                  gatsbyImageData
+               }
+            }
          }
          body
       }
